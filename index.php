6498cc35081fa9b031f25362b9d24db4a9f9657a
bilkicki <50716908+bilkicki@users.noreply.github.com>
2019-05-19 06:32:46 -0500

    initial

---
+++ index.php
@@ -0,0 +1,42 @@
+<script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
+
+<nav>
+  <ul id="navbar">
+    <li><a href="#welcome-section">
+      welcome</a></li>
+    <li><a href="#projects">
+      projects</a></li>
+    <li><a href="#contact">
+      contact</a></li>
+  </ul>
+</nav>
+<main>
+  <section id="welcome-section">
+    <h1>Hi I'm Josh</h1>
+    <p>a programmer.</p>
+  </section>
+  <section id="projects">
+    <div>
+      <h2>Here are some of my projects</h2>
+      <p>and other achievements:</p>
+    </div>
+    <div class="project-tile">
+      <a href="https://codepen.io/bilkicki/pen/KLmmmr"></a>
+    </div>
+  </section>
+  <section id="contact">
+    <div>
+      <h2>Let's work together...</h2>
+      <p>we can have coffee!</p>
+    </div>
+    <div id="social">
+      <a href="https://twitter.com/JoshuaKielbicki" target="_blank" class="social-links">Twitter</a>
+      <a href="ps://www.facebook.com/joshuakielbicki" target="_blank" class="social-links">Facebook</a>
+      <a id="profile-link" href="https://github.com/bilkicki" target="_blank" class="social-links">GitHub</a>
+      <a href="mailto:bilkicki@gmail.com" class="social-links">email me</a>
+    </div>
+  </section>
+</main>
+<footer>
+  <p>&copy <a href="https://www.imjosh.ga">Joshua Kielbicki</a></p>
+</footer>
